c74df808b004ce206839664376ce5d7ae1f9d54d
edwardzxy <1649239969@qq.com>
2019-03-16 23:10:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,10 @@ import RPi.GPIO as GPIO
 # BOARD编号方式，基于插座引脚编号
 GPIO.setmode(GPIO.BOARD)
 
+GPIO.setup(45, GPIO.OUT)
+GPIO.setup(44, GPIO.OUT)
+GPIO.setup(60, GPIO.OUT)
+
 #os.system('rm -f tmp.txt')
 
 #Setup I/O (Green = 45, Red = 60)
