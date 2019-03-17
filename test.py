8c89c6ce530591bf98c7433a0a4f52bc2a98487e
edwardzxy <1649239969@qq.com>
2019-03-17 10:36:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,67 +6,65 @@ import time
 import signal
 import RPi.GPIO as GPIO
 
-
 GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.OUT)
 
+# os.system('rm -f tmp.txt')
 
-#os.system('rm -f tmp.txt')
+# Setup I/O (Green = 45, Red = 60)
+# Door = 44
 
-#Setup I/O (Green = 45, Red = 60)
-#Door = 44
-
-#os.system('echo 44 > /sys/class/gpio/export')
-#os.system('echo out > /sys/class/gpio/gpio44/direction')
-#os.system('echo 0 > /sys/class/gpio/gpio44/value')
+# os.system('echo 44 > /sys/class/gpio/export')
+# os.system('echo out > /sys/class/gpio/gpio44/direction')
+# os.system('echo 0 > /sys/class/gpio/gpio44/value')
 #
-#os.system('echo 45 > /sys/class/gpio/export')
-#os.system('echo out > /sys/class/gpio/gpio45/direction')
-#os.system('echo 0 > /sys/class/gpio/gpio45/value')
+# os.system('echo 45 > /sys/class/gpio/export')
+# os.system('echo out > /sys/class/gpio/gpio45/direction')
+# os.system('echo 0 > /sys/class/gpio/gpio45/value')
 #
-#os.system('echo 60 > /sys/class/gpio/export')
-#os.system('echo out > /sys/class/gpio/gpio60/direction')
-#os.system('echo 1 > /sys/class/gpio/gpio60/value')
+# os.system('echo 60 > /sys/class/gpio/export')
+# os.system('echo out > /sys/class/gpio/gpio60/direction')
+# os.system('echo 1 > /sys/class/gpio/gpio60/value')
 
 
-
-#if there's a valid file here, delete it
+# if there's a valid file here, delete it
 os.system('rm -f valid.txt')
-#in a while loop, compare current tmp.txt QR hash with the approved hash from the web
+# in a while loop, compare current tmp.txt QR hash with the approved hash from the web
 
 
 while (1):
-    os.system('wget --quiet https://raw.githubusercontent.com/edwardzxy/qrtest/master/valid.txt')
-    
-    approved = subprocess.check_output(["tail", "-n", "1", "valid.txt"])
-    print "Approved Hash from Web: %s" % approved
-    os.system('rm valid.txt')
-    #
-    os.system("raspistill -w 320 -h 240 -o image.jpg -t 3")
-    zbarcam=subprocess.Popen("zbarimg --raw image.jpg", stdout=subprocess.PIPE, shell=True,preexec_fn=os.setsid)
-    qrcodetext=zbarcam.stdout.readline()
-    os.system('rm image.jpg')
-    #
-    if approved in qrcodetext:
-        print "QR Code Accepted"
-        GPIO.output(18, GPIO.LOW)
+    for i in rang(1,10)
+        os.system('wget --quiet https://raw.githubusercontent.com/edwardzxy/qrtest/master/valid.txt')
 
-        #os.system('echo 1 > /sys/class/gpio/gpio45/value')
-        #os.system('echo 1 > /sys/class/gpio/gpio44/value')
-        #os.system('echo 0 > /sys/class/gpio/gpio60/value')
-        time.sleep(10)
-        GPIO.output(18, GPIO.HIGH)
+        approved = subprocess.check_output(["tail", "-n", "1", "valid.txt"])
+        print "Approved Hash from Web: %s" % approved
+        os.system('rm valid.txt')
+        #
+        os.system("raspistill -w 320 -h 240 -o image.jpg -t 3")
+        zbarcam = subprocess.Popen("zbarimg --raw image.jpg", stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+        qrcodetext = zbarcam.stdout.readline()
+        os.system('rm image.jpg')
+        #
+        if approved in qrcodetext:
+            print "QR Code Accepted"
+            GPIO.output(18, GPIO.LOW)
 
-        #os.system('echo 0 > /sys/class/gpio/gpio45/value')
-        #os.system('echo 0 > /sys/class/gpio/gpio44/value')
-        #os.system('echo 1 > /sys/class/gpio/gpio60/value')
-    else:
-        print "No Valid Code Presented"
-        GPIO.output(18, GPIO.HIGH)
+            # os.system('echo 1 > /sys/class/gpio/gpio45/value')
+            # os.system('echo 1 > /sys/class/gpio/gpio44/value')
+            # os.system('echo 0 > /sys/class/gpio/gpio60/value')
+            time.sleep(10)
+            GPIO.output(18, GPIO.HIGH)
 
-        #os.system('echo 1 > /sys/class/gpio/gpio60/value')
-        #os.system('echo 0 > /sys/class/gpio/gpio44/value')
-        #os.system('echo 0 > /sys/class/gpio/gpio45/value')
+            # os.system('echo 0 > /sys/class/gpio/gpio45/value')
+            # os.system('echo 0 > /sys/class/gpio/gpio44/value')
+            # os.system('echo 1 > /sys/class/gpio/gpio60/value')
+        else:
+            print "No Valid Code Presented"
+            GPIO.output(18, GPIO.HIGH)
+
+            # os.system('echo 1 > /sys/class/gpio/gpio60/value')
+            # os.system('echo 0 > /sys/class/gpio/gpio44/value')
+            # os.system('echo 0 > /sys/class/gpio/gpio45/value')
 
